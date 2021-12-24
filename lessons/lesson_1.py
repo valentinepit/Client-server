2019818b663897771d602283ae2902127ad3cd73
trmcreates <admin@payperyou.com>
2021-12-24 14:05:18 +0500

    Initial commit

---
+++ lessons/lesson_1.py
@@ -0,0 +1,81 @@
+def task_1():
+    words = {"разработка": "\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430",
+             "сокет": "\u0441\u043e\u043a\u0435\u0442",
+             "декоратор": "\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440",
+             }
+    for item in words:
+        print(f"Слово - {item} *** Тип -{type(item)}\n В UTF-8 \n Слово - {words[item]} *** Тип - {type(words[item])}")
+
+
+def task_2():
+    words = ["class", "function", "method"]
+    for word in words:
+        print(eval("b'word'"))
+
+
+def task_3():
+    flag = False
+    words = ["attribute", "класс", "функция", "type"]
+    for word in words:
+        uni_word = word.encode("utf-8")
+        for letter in word:
+            if ord(letter) > 127:
+                flag = True
+                break
+        if flag:
+            print(f"слово {word} нельзя представить в байтовом виде {uni_word}")
+        else:
+            print(f"Перекодировка показала {word} в байтовом виде {uni_word}")
+
+
+def task_4():
+    words = ["разработка",
+             "администрирование",
+             "protocol",
+             "standard",
+             ]
+    for word in words:
+        print(f"Слово {word} в байтовом виде: {word.encode('utf-8')}")
+        print(f"Обратная перекодировка: {word.encode('utf-8').decode('utf-8')}")
+
+
+def task_5():
+    import subprocess
+    import chardet
+    import platform
+    targets = ["yandex.ru",
+               "youtube.com",
+               ]
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    for target in targets:
+        args = ['ping', param, '2', target]
+        subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+        for line in subproc_ping.stdout:
+            result = chardet.detect(line)
+            line = line.decode(result['encoding']).encode('utf-8')
+            print(line.decode('utf-8'))
+
+
+def task_6():
+    name = "test.txt"
+    encoding = file_encoding_detect(name)
+    with open(name, encoding=encoding) as f:
+        content = f.read()
+    print(content)
+
+
+def file_encoding_detect(_name):
+    from chardet import detect
+    with open(_name, 'rb') as f:
+        content = f.read()
+    encoding = detect(content)['encoding']
+    return encoding
+
+
+if __name__ == "__main__":
+    # task_1()
+    task_2()
+    # task_3()
+    # task_4()
+    # task_5()
+    # task_6()
